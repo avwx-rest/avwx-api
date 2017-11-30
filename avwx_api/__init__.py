f30d0bc5ea88ff843963ac533944aa7a599566df
Michael duPont <michael@mdupont.com>
2017-11-29 22:15:56 -0500

    Add CORS header

--- avwx_api/__init__.py
+++ avwx_api/__init__.py
@@ -4,7 +4,11 @@ avwx_api.__init__ - High-level Flask application
 """
 
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={
+    r'/api/*': {'origins': '*'}
+})
 
 import avwx_api.views
