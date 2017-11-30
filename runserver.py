95f39c58447be9796b6d4e3323207a675b2369ab
Michael duPont <michael@mdupont.com>
2017-11-29 22:14:35 -0500

    Run on port 8000

--- runserver.py
+++ runserver.py
@@ -5,4 +5,4 @@ This script runs the AVWX application using a development server.
 from avwx_api import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8000)
