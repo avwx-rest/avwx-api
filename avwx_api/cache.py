0cb84a7312121a5d3211c316782b6e579b4af29b
Michael duPont <michael@mdupont.com>
2017-12-17 20:51:10 -0500

    Disable cache functions if no URI found

--- avwx_api/cache.py
+++ avwx_api/cache.py
@@ -15,6 +15,8 @@ class Cache(object):
     """Controls connections with the MongoDB-compatible document cache"""
 
     def __init__(self):
+        if not MONGO_URI:
+            return
         db = pymongo.MongoClient(MONGO_URI).report_cache
         self.tables = {
             'metar': db.metar,
@@ -32,11 +34,15 @@ class Cache(object):
         By default, will only return if the cache timestamp has not been exceeded
         Can force the cache to return if force is True
         """
+        if not MONGO_URI:
+            return
         data = self.tables[rtype].find_one({'_id': station})
         if force or (isinstance(data, dict) and not self.has_expired(data['timestamp'])):
             return data
 
     def update(self, rtype: str, data: {str: object}):
         """Update the cache"""
+        if not MONGO_URI:
+            return
         data['timestamp'] = datetime.utcnow()
         self.tables[rtype].update({'_id': data['data']['Station']}, data, upsert=True)
