3564f8fa63ac1104493dabbd7186877daa2362c0
Michael duPont <michael@mdupont.com>
2019-02-07 23:49:06 -0500

    jsonify converts date objects to ISO format

--- avwx_api/__init__.py
+++ avwx_api/__init__.py
@@ -4,15 +4,32 @@ avwx_api.__init__ - High-level Quart application
 """
 
 # stdlib
+from datetime import date
 from os import environ, path
 # library
 from quart import got_request_exception
+from quart.json import JSONEncoder
 from quart_openapi import Pint
 import rollbar
 # from rollbar.contrib.quart import report_exception
 from avwx_api.rollbar_handler import report_exception
 
 app = Pint(__name__)
+
+class CustomJSONEncoder(JSONEncoder):
+
+    def default(self, obj):
+        try:
+            if isinstance(obj, date):
+                return obj.isoformat()
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
+
+app.json_encoder = CustomJSONEncoder
 
 @app.before_first_request
 def init_rollbar():
