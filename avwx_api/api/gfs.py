987fdeeb4e1f79a377239b5f21ec79c444f50981
flyinactor91 <michael@mdupont.com>
2020-04-04 18:00:17 -0400

    GFS now live

--- avwx_api/api/gfs.py
+++ avwx_api/api/gfs.py
@@ -10,26 +10,26 @@ from avwx_api.api.base import Report, Parse
 @app.route("/api/gfs/mav/<station>")
 class Mav(Report):
     report_type = "mav"
-    # plan_types = ("pro", "enterprise")
+    plan_types = ("pro", "enterprise")
     handler = handle.MavHandler
 
 
 @app.route("/api/parse/gfs/mav")
 class MavParse(Parse):
     report_type = "mav"
-    # plan_types = ("pro", "enterprise")
+    plan_types = ("pro", "enterprise")
     handler = handle.MavHandler
 
 
 @app.route("/api/gfs/mex/<station>")
 class Mex(Report):
     report_type = "mex"
-    # plan_types = ("pro", "enterprise")
+    plan_types = ("pro", "enterprise")
     handler = handle.MexHandler
 
 
 @app.route("/api/parse/gfs/mex")
 class MexParse(Parse):
     report_type = "mex"
-    # plan_types = ("pro", "enterprise")
+    plan_types = ("pro", "enterprise")
     handler = handle.MexHandler
