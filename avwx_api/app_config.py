2279a6ec4e80d05abc812cd7641eb622bc54557c
Michael duPont <flyinactor91@gmail.com>
2023-10-30 11:36:02 -0400

    Updated app init and versions

--- avwx_api/app_config.py
+++ avwx_api/app_config.py
@@ -16,7 +16,7 @@ from rollbar.contrib.quart import report_exception
 
 # module
 from avwx import exceptions as avwx_exceptions
-from avwx_api_core.app import add_cors, CustomJSONEncoder
+from avwx_api_core.app import add_cors, CustomJSONProvider
 from avwx_api_core.cache import CacheManager
 from avwx_api_core.token import TokenManager
 from avwx_api.station_counter import StationCounter
@@ -30,24 +30,10 @@ MONGO_URI = environ.get("MONGO_URI")
 
 
 app = Pint(__name__)
-app.json_encoder = CustomJSONEncoder
+app.json_provider_class = CustomJSONProvider
 app.after_request(add_cors)
 
 
-@app.before_serving
-async def init_helpers():
-    """Init API helpers
-
-    Need async to connect helpers to event loop
-    """
-    app.mdb = AsyncIOMotorClient(MONGO_URI) if MONGO_URI else None
-    app.cache = CacheManager(app, expires=CACHE_EXPIRES)
-    app.token = TokenManager(app)
-    app.station = StationCounter(app)
-    app.cache_only = {}
-
-
-@app.before_first_request
 def init_rollbar():
     """Initialize Rollbar exception logging"""
     key = environ.get("LOG_KEY")
@@ -62,7 +48,6 @@ def init_rollbar():
     avwx_exceptions.exception_intercept = exception_intercept
 
 
-@app.before_first_request
 async def init_cache_only_map():
     """Fetch cache-only station lists for the duration of the worker"""
     if app.mdb is None:
@@ -70,3 +55,18 @@ async def init_cache_only_map():
     for table in ("awos",):
         codes = await app.mdb.cache[table].distinct("_id")
         app.cache_only.update({code: table for code in codes})
+
+
+@app.before_serving
+async def init_helpers():
+    """Init API helpers
+
+    Need async to connect helpers to event loop
+    """
+    app.mdb = AsyncIOMotorClient(MONGO_URI) if MONGO_URI else None
+    app.cache = CacheManager(app, expires=CACHE_EXPIRES)
+    app.token = TokenManager(app)
+    app.station = StationCounter(app)
+    app.cache_only = {}
+    init_rollbar()
+    await init_cache_only_map()
